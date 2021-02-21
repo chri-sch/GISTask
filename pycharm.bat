3e5cdc816ae3a9ae3781cc1ce4a44f4a5ec66bbb
Christoph Schlager <c.schlager@trafility.at>
2021-02-21 20:09:55 +0100

    changed to myUser

--- pycharm.bat
+++ pycharm.bat
@@ -11,4 +11,4 @@ rem Set VSI cache to be used as buffer, see #6448
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
 set QT_PLUGIN_PATH=C:\Program Files\QGIS 3.12\apps\qgis\qtplugins;C:\Program Files\QGIS 3.12\apps\qt5\plugins
-start /d "C:\Users\chsc\AppData\Local\JetBrains\PyCharm Community Edition 2019.3.2\bin" pycharm64.exe
+start /d "C:\Users\myUser\AppData\Local\JetBrains\PyCharm Community Edition 2019.3.2\bin" pycharm64.exe
